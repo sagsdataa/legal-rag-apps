0df9cbecabe7494768cd87d0e46c102837826cbe
sagsdataa <sagsdataa@gmail.com>
2026-08-24 20:17:41 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,30 +1,11 @@
 import streamlit as st
+import os
 
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+st.write("Current Directory:")
+st.write(os.getcwd())
 
-st.title("Legal RAG Assistant")
+st.write("Files in Current Directory:")
+st.write(os.listdir("."))
 
-st.write("Step 1: Start")
-
-@st.cache_resource
-def load_embeddings():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
-
-embeddings = load_embeddings()
-
-st.write("✅ Step 2: Embeddings Loaded")
-
-@st.cache_resource
-def load_vector_db():
-    return FAISS.load_local(
-        "../faiss_index",
-        embeddings,
-        allow_dangerous_deserialization=True
-    )
-
-vectorDB = load_vector_db()
-
-st.write("✅ Step 3: FAISS Loaded")
+st.write("Parent Directory:")
+st.write(os.listdir(".."))
