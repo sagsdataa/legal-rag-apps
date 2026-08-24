8d61e85b2bff997200bf7b0fe31de7d475fc0111
sagsdataa <sagsdataa@gmail.com>
2026-08-24 21:21:33 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -53,7 +53,7 @@ def load_llm():
 
     pipe = pipeline(
         "text-generation",
-        model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+        model="microsoft/Phi-3-mini-4k-instruct",
         max_new_tokens=128
     )
 
